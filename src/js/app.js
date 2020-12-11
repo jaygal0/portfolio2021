81197723489c64b22608c94ade0741f2c6a02629
Joshua Galinato <joshuagalinato@gmail.com>
2020-12-11 11:28:33 +0100

    installing polyfill

--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,11 @@
 import barba, { HookMethods } from '@barba/core'
-import barbaCss from '@barba/css'
+// import barbaCss from '@barba/css'
 
-// tell Barba to use the css plugin
-barba.use(barbaCss)
+// // tell Barba to use the css plugin
+// barba.use(barbaCss)
 
 // register GSAP ScrollTrigger
 gsap.registerPlugin(ScrollTrigger)
-
-// Wide Card animations
-gsap.from('.widecard', {
-  scrollTrigger: {
-    trigger: '.widecard',
-    toggleActions: 'restart none none none',
-    // start: 'center bottom',
-    // end: 'center top',
-    // markers: true,
-  },
-  duration: 1,
-  opacity: 0,
-  scale: 0.9,
-  y: '19%',
-  ease: 'elastic',
-  stagger: 0.5,
-})
-
-gsap.from('.singleCard', {
-  scrollTrigger: {
-    trigger: '.singleCard',
-    toggleActions: 'restart none none none',
-    // start: 'center bottom',
-    // end: 'center top',
-    // markers: true,
-  },
-  duration: 1,
-  opacity: 0,
-  scale: 0.9,
-  y: '19%',
-  ease: 'elastic',
-  stagger: 0.5,
-})
 
 barba.hooks.enter(() => {
   window.scrollTo(0, 0)
@@ -49,43 +16,105 @@ barba.init({
   sync: true,
   transitions: [
     {
-      name: 'home',
-      once() {},
-      leave() {},
-      enter() {},
-    },
-    {
-      name: 'cover',
-      to: {
-        namespace: ['home', 'about', 'work', 'skills', 'detail'],
-      },
-      leave(data) {
+      async leave(data) {
+        transition()
+        await delay(1000)
         data.current.container.remove()
-        // await delay(1000)
       },
-      beforeEnter(data) {
+      async beforeEnter(data) {
         ScrollTrigger.getAll().forEach((t) => t.kill())
       },
-      enter(data) {},
+      async enter(data) {
+        wideCards()
+        singleCards()
+        content()
+
+        console.log('entering')
+        // transition()
+      },
+      once(data) {
+        wideCards()
+      },
     },
   ],
 })
 
-// const card = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: '.widecard .singleCard',
-//     start: 'center bottom',
-//     toggleActions: 'restart none none none',
-//   },
-//   defaults: {
-//     duration: 1,
-//     opacity: 0,
-//     scale: 0.9,
-//     y: '10%',
-//     ease: 'elastic',
-//   },
-// })
+function transition() {
+  const tl = gsap.timeline()
 
-// card
-//   .from('.widecard', { stagger: 0.2 })
-//   .from('.singleCard', { stagger: 0.2 }, '-=.5')
+  tl.to('.transition li', {
+    duration: 0.8,
+    scaleX: 1,
+    transformOrigin: 'left',
+    stagger: 0.2,
+  })
+
+  tl.to('.transition li', {
+    duration: 1,
+    scaleX: 0,
+    transformOrigin: 'right',
+    stagger: 0.1,
+    delay: 0.1,
+  })
+}
+
+function wideCards() {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.widecard',
+      markers: true,
+      start: 'top 85%',
+      end: 'bottom 60%',
+      toggleActions: 'restart none none none',
+    },
+  })
+
+  tl.from('.widecard', {
+    duration: 1,
+    opacity: 0,
+    scale: 0.9,
+    y: '19%',
+    ease: 'elastic',
+    stagger: 0.5,
+  })
+}
+
+function singleCards() {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.singleCard',
+      markers: true,
+      start: 'top 85%',
+      end: 'bottom 60%',
+      toggleActions: 'restart none none none',
+    },
+  })
+
+  tl.from('.singleCard', {
+    duration: 1,
+    opacity: 0,
+    scale: 0.9,
+    y: '19%',
+    ease: 'elastic',
+    stagger: 0.5,
+  })
+}
+
+function content() {
+  const tl = gsap.timeline()
+
+  tl.from('.container', {
+    opacity: 0,
+    duration: 2,
+    delay: 1,
+  })
+}
+
+function delay(n) {
+  n = n || 2000
+  return new Promise((done) => {
+    setTimeout(() => {
+      done()
+    }, n)
+  })
+}
